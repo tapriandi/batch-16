292ccd35f91ff60e19007a80d3d3eece7ada34fc
tapriandi <tapriandi@gmail.com>
2023-11-04 15:51:25 +0700

    latihan javascript day-2

---
+++ script.js
@@ -0,0 +1,9 @@
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const numbers = [100, 10, 1, 44, 10000, 12]
+
+console.log(numbers.sort())
+
+
+// 1,10,12,44,100,1000
+// tetap / tidak berubah
+// 1, 10,100,1000,12,44
